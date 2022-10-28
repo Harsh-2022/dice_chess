648fa589fc5ddf8cb23f0afb71fc9754f61cdbff
om-ghumare <omprashant20bcs130@iiitkottayam.ac.in>
2022-10-28 10:45:12 +0530

    dice added

--- chess0_1.py
+++ chess0_1.py
@@ -1,6 +1,11 @@
+from cgitb import small
 from cmath import rect
 from turtle import pos
 import pygame  
+import random
+
+
+roll_num=3
 
 
 FPS = 30 # frames per second setting
@@ -13,8 +18,35 @@ width = screen.get_width()
 height = screen.get_height()
 smallfont = pygame.font.SysFont('comicsansms',35)
 
+
+
 sq_size=(height-60)/8
 p_size=sq_size-17
+
+
+pawn_1=pygame.image.load('images/dice and frame/PAWN.png')
+king_2=pygame.image.load('images/dice and frame/KING.png')
+bishop_3=pygame.image.load('images/dice and frame/BISHOP.png')
+knight_4=pygame.image.load('images/dice and frame/KNIGHT.png')
+rook_5=pygame.image.load('images/dice and frame/ROOK.png')
+queen_6=pygame.image.load('images/dice and frame/QUEEN.png')
+cor_1=pygame.image.load('images/dice and frame/cor1.png')
+cor_2=pygame.image.load('images/dice and frame/cor2.png')
+cor_3=pygame.image.load('images/dice and frame/cor3.png')
+cor_4=pygame.image.load('images/dice and frame/cor4.png')
+
+pawn_1=pygame.transform.scale(pawn_1,(300,300))
+king_2=pygame.transform.scale(king_2,(300,300))
+bishop_3=pygame.transform.scale(bishop_3,(300,300))
+knight_4=pygame.transform.scale(knight_4,(300,300))
+rook_5=pygame.transform.scale(rook_5,(300,300))
+queen_6=pygame.transform.scale(queen_6,(300,300))
+cor_1=pygame.transform.scale(cor_1,(300,300))
+cor_2=pygame.transform.scale(cor_2,(300,300))
+cor_3=pygame.transform.scale(cor_3,(300,300))
+cor_4=pygame.transform.scale(cor_4,(300,300))
+
+
 
 b_k=pygame.image.load('images/black_k.png')
 b_n=pygame.image.load('images/black_n.png')
@@ -44,25 +76,27 @@ w_b=pygame.transform.scale(w_b,(p_size,p_size))
 w_q=pygame.transform.scale(w_q,(p_size,p_size))
 w_r=pygame.transform.scale(w_r,(p_size,p_size))
 
-class Piece:
+
+
+# class Piece:
  
-    def __init__(self, chess_colour):
-        self._chess_colour = chess_colour
-        self._is_active = True
+#     def __init__(self, chess_colour):
+#         self._chess_colour = chess_colour
+#         self._is_active = True
  
-    # chess_colour getter
-    @property
-    def chess_colour(self):
-        return self._chess_colour
+#     # chess_colour getter
+#     @property
+#     def chess_colour(self):
+#         return self._chess_colour
  
-    # is_active setter/getter
-    @property
-    def is_active(self):
-        return self._is_active
+#     # is_active setter/getter
+#     @property
+#     def is_active(self):
+#         return self._is_active
  
-    @is_active.setter
-    def is_active(self, value):
-        self._is_active = value
+#     @is_active.setter
+#     def is_active(self, value):
+#         self._is_active = value
 
 def set_peice(x,y,isWhite,pos):
     x=x+8
@@ -164,9 +198,55 @@ def chesscanvas():
     screen.blit(player2,(board_size+50,board_size-20))
 
 
-def show_score(score):
-    Scoring=smallfont.render(str(score),True,"white")
-    screen.blit(Scoring, (925,500))
+# def show_score(score):
+#     Scoring=smallfont.render(str(score),True,"white")
+#     screen.blit(Scoring, (925,500))
+
+
+
+
+# def dice_sect(roll_num):
+#     #roll_num=1
+#     dice_show(roll_num)
+
+
+def dice_show(roll_num):        
+    
+    pygame.draw.rect(screen,'grey',pygame.Rect(width-458,height/2-100,200,200))
+    screen.blit(pawn_1,(width-500,height/2-150))
+    if roll_num==1:
+        screen.blit(pawn_1,(width-500,height/2-150))
+        print(roll_num)
+    if roll_num==2:
+        print(roll_num)
+        screen.blit(king_2,(width-500,height/2-150))
+    if roll_num==3:
+        print(roll_num)
+        screen.blit(bishop_3,(width-500,height/2-150))
+    if roll_num==4:
+        print(roll_num)
+        screen.blit(knight_4,(width-500,height/2-150))
+    if roll_num==5:
+        print(roll_num)
+        screen.blit(rook_5,(width-500,height/2-150))
+    if roll_num==6:
+        print(roll_num)
+        screen.blit(queen_6,(width-500,height/2-150))
+    
+
+def dice_roll():
+    global roll_num
+    roll_num=random.randint( 1,6 )
+
+    
+    # pygame.draw.rect(screen, "red", pygame.Rect(900,500,20,50))
+    print("rolling")
+    #print(roll_num)
+    # screen.blit(cor_1,(width-500,height/2-150))
+    # screen.blit(cor_2,(width-500,height/2-150))
+    # screen.blit(cor_3,(width-500,height/2-150))
+    # screen.blit(cor_4,(width-500,height/2-150))
+    dice_show(roll_num)
 
 def main():
     score=0
@@ -179,6 +259,8 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
+                if width-415 <= mouse[0] <= width-295 and height/2+120 <= mouse[1] <= height/2+180:
+                    dice_roll()
                 if width-85 <= mouse[0] <= width-5 and 7 <= mouse[1] <= 60:
                     running = False
             # elif event.type == pygame.MOUSEBUTTONDOWN:
@@ -186,24 +268,37 @@ def main():
                     running=False
                     main()
 
-            # elif event.type == pygame.MOUSEBUTTONDOWN:
-                if 900<=mouse[0]<=920 and 500 <=mouse[1]<=550:
-                    score=score+1
-    
+                  
+ 
                
 
             
             mouse = pygame.mouse.get_pos()
-
-
+            global roll_p1
+            global roll_p2
             global Quit 
             global rematch
+
+            
+            if width-415 <= mouse[0] <= width-295 and (height/2)+120 <= mouse[1] <= (height/2)+180:
+                roll_p1=smallfont.render('Roll',True,'green')
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+            else:
+                roll_p1=smallfont.render('Roll',True,'white')
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+            if width-415 <= mouse[0] <= width-295 and (height/2)-180 <= mouse[1] <= (height/2)-120:
+                roll_p2=smallfont.render('Roll',True,'green')
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+            else:
+                roll_p2=smallfont.render('Roll',True,'white')
+            
+
             if width-85 <= mouse[0] <= width-5 and 7 <= mouse[1] <= 60:
                 Quit = smallfont.render('Exit' , True , "yellow")
                 pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
             else:
                 Quit = smallfont.render('Exit' , True , "white")
-                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                # pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
             if width-285<= mouse[0] <= width-90 and 7 <= mouse[1] <= 60:
                 rematch=smallfont.render('Play Again',True,"yellow")
@@ -212,15 +307,24 @@ def main():
                 rematch=smallfont.render('Play Again',True,"white")
 
         # pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+
+        back_ground_color=pygame.Color(0,0,0)
+        screen.fill(back_ground_color)
+        screen.blit(roll_p1 , (width-390,height/2+126))
+        screen.blit(roll_p2 , (width-390,height/2-166))
         screen.blit(Quit , (width-80,10))
         screen.blit(rematch , (width-280,10))
-        show_score(score)
+        # show_score(score)
+
                            
         
         chesscanvas()
+    
+        dice_show(roll_num)
+        # pygame.draw.rect(screen,'grey',pygame.Rect(width-415,height/2+120,120,60))-------------------------
+        # dice_show()
         
-        
-        pygame.draw.rect(screen, "red", pygame.Rect(900,500,20,50))
+        # pygame.draw.rect(screen, "red", pygame.Rect(900,500,20,50))
 
 
 
